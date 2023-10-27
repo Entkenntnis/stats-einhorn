9517e31a37d591f2303a51efdedca2b301febc77
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-10-27 18:03:54 +0200

    add check route

--- src/routes/check.js
+++ src/routes/check.js
@@ -1,6 +1,18 @@
-/*module.exports = (App) => {
-  App.express.post('/check', async (req, res) => {
-    
-  }
+const { safeHandler } = require('../lib/safeHandler')
+
+module.exports = (App) => {
+  App.express.post(
+    '/check',
+    safeHandler(async (req, res) => {
+      const { name } = req.body
+
+      if (typeof name !== 'string') {
+        return res.json({ ok: false, reason: 'Parameter Name fehlt.' })
+      }
+
+      const existingUser = App.db.User.findOne({ name })
+
+      return res.json({ ok: true, userExists: !!existingUser })
+    })
+  )
 }
-*/
